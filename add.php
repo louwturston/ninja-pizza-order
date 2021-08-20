391e0ea6198c86c2db9ec9742cf06d379472ceb5
Turston Louw <83589621+louwturston@users.noreply.github.com>
2021-08-20 14:32:36 +0200

    Update add.php

--- add.php
+++ add.php
@@ -1,10 +1,10 @@
-<?php
+<?php 
 
-	//if(isset($_GET['submit'])){
-	//	echo $_GET['email'] . '<br />';
-	//	echo $_GET['title'] . '<br />';
-	//	echo $_GET['ingredients'] . '<br />';
-	//}
+	// if(isset($_GET['submit'])){
+	// 	echo $_GET['email'] . '<br />';
+	// 	echo $_GET['title'] . '<br />';
+	// 	echo $_GET['ingredients'] . '<br />';
+	// }
 
 	if(isset($_POST['submit'])){
 		echo $_POST['email'] . '<br />';
@@ -16,6 +16,7 @@
 
 <!DOCTYPE html>
 <html>
+	
 	<?php include('templates/header.php'); ?>
 
 	<section class="container grey-text">
@@ -25,13 +26,14 @@
 			<input type="text" name="email">
 			<label>Pizza Title</label>
 			<input type="text" name="title">
-			<label>Ingredients (comma seperated)</label>
+			<label>Ingredients (comma separated)</label>
 			<input type="text" name="ingredients">
-			<div class="center"
-			<input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+			<div class="center">
+				<input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
 			</div>
 		</form>
 	</section>
 
 	<?php include('templates/footer.php'); ?>
+
 </html>
