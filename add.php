82b3ba4c4d283365cd0a9eb40d00ef7ca7ced5c5
Turston Louw <83589621+louwturston@users.noreply.github.com>
2021-08-23 12:04:56 +0200

    Update add.php

--- add.php
+++ add.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+
+	include('config/db_connect.php');
 
 	$email = $title = $ingredients = '';
 	$errors = array('email' => '', 'title' => '', 'ingredients' => '');
@@ -34,12 +36,26 @@
 				$errors['ingredients'] = 'Ingredients must be a comma separated list';
 			}
 		}
-		
+
 		if(array_filter($errors)){
 			//echo 'errors in form';
-		}else{
-			//echo 'form is valid';
-			header('Location: index.php');
+		} else {
+			// escape sql chars
+			$email = mysqli_real_escape_string($conn, $_POST['email']);
+			$title = mysqli_real_escape_string($conn, $_POST['title']);
+			$ingredients = mysqli_real_escape_string($conn, $_POST['ingredients']);
+
+			// create sql
+			$sql = "INSERT INTO pizzas(title,email,ingredients) VALUES('$title','$email','$ingredients')";
+
+			// save to db and check
+			if(mysqli_query($conn, $sql)){
+				// success
+				header('Location: index.php');
+			} else {
+				echo 'query error: '. mysqli_error($conn);
+			}
+			
 		}
 
 	} // end POST check
